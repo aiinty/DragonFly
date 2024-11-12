4d58d086d2b2fa29d1603f4aa8bfc0f855337489
clovexx <clovexx.github@gmail.com>
2024-11-12 20:45:21 +0600

    chore: add profile screen

---
+++ app/src/main/java/com/aiinty/dragonfly/ui/screens/ProfileScreen.kt
@@ -0,0 +1,20 @@
+package com.aiinty.dragonfly.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.aiinty.dragonfly.core.entity.User
+
+object Profile;
+
+@Composable
+fun ProfileScreen(
+    user: User
+) {
+
+}
+
+@Preview
+@Composable
+fun ProfileScreenPreview() {
+
+}
