dda655ed2fb51374a859255a2ac59d9d61cbb42f
clovexx <clovexx.github@gmail.com>
2024-11-16 00:09:32 +0600

    chore(header component): removed padding

--- app/src/main/java/com/aiinty/dragonfly/ui/components/BaseHeader.kt
+++ app/src/main/java/com/aiinty/dragonfly/ui/components/BaseHeader.kt
@@ -3,12 +3,10 @@ package com.aiinty.dragonfly.ui.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun BaseHeader(
@@ -17,7 +15,7 @@ fun BaseHeader(
 ) {
     Surface {
         Row(
-            modifier = modifier.fillMaxWidth().padding(16.dp),
+            modifier = modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = if (items.size == 1) Arrangement.Center else Arrangement.SpaceBetween,
         ) {
