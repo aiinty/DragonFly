145a18e999bbdba59857aa841588f2ff4c59691f
clovexx <clovexx.github@gmail.com>
2024-11-15 23:34:07 +0600

    chore(theme): removed color duplicate

--- app/src/main/java/com/aiinty/dragonfly/ui/components/PocketSection.kt
+++ app/src/main/java/com/aiinty/dragonfly/ui/components/PocketSection.kt
@@ -6,14 +6,12 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -23,15 +21,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.aiinty.dragonfly.R
-import com.aiinty.dragonfly.ui.theme.ECECEC
+import com.aiinty.dragonfly.ui.theme.Outline
 import com.aiinty.dragonfly.ui.theme.PrimaryContainer
 
 @Composable
@@ -39,15 +35,6 @@ fun PocketSection(items: List<@Composable () -> Unit>, viewMore: Boolean = true,
     Column(
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.spacedBy(8.dp), verticalAlignment = Alignment.CenterVertically) {
-            Icon(imageVector = ImageVector.vectorResource(R.drawable.pocket), stringResource(R.string.my_pocket), tint = PrimaryContainer)
-            Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
-                Text(stringResource(R.string.my_pocket))
-                TextButton({}, contentPadding = PaddingValues(0.dp)) {
-                    Text(stringResource(R.string.create_pocket), lineHeight = 18.sp, fontSize = 12.sp, color = PrimaryContainer)
-                }
-            }
-        }
 
         items.chunked(2).forEach {  rowItems ->
             Row(
@@ -85,7 +72,7 @@ fun PocketSection(items: List<@Composable () -> Unit>, viewMore: Boolean = true,
 
 @Composable
 fun PocketCard(title: String, @DrawableRes iconId: Int) {
-    Column(Modifier.border(1.dp, ECECEC, RoundedCornerShape(8.dp))) {
+    Column(Modifier.border(1.dp, Outline, RoundedCornerShape(8.dp))) {
         Image(
             ImageBitmap.imageResource(iconId), stringResource(R.string.card), Modifier
             .size(164.dp, 150.dp)
